7afaec2b70313d700872424552d34df2e016063c
bapl <bapl1016@gmail.com>
2017-09-04 00:16:32 +0800

    refactor file

--- mockito/src/main/java/bapl/mockito/powermock/PrivatePartialMockingExample.java
+++ mockito/src/main/java/bapl/mockito/powermock/PrivatePartialMockingExample.java
@@ -1,5 +1,11 @@
 package bapl.mockito.powermock;
 
 public class PrivatePartialMockingExample {
-
+	 public String methodToTest() { 
+	       return methodToMock("input"); 
+	   } 
+	 
+	   private String methodToMock(String input) { 
+	       return "REAL VALUE = " + input; 
+	   } 
 }
